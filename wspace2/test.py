060b7f410a397fe94b1db84abb37937e090a9503
Mauro Adauto <mauroadauto9@gmail.com>
2023-11-05 20:17:11 -0500

    Final Commit - Both pages works with the model

--- wspace2/test.py
+++ wspace2/test.py
@@ -7,8 +7,6 @@ import pandas as pd
 import tensorflow as tf
 import numpy as np
 
-app = Flask(__name__)
-
 json_folder = os.path.join(os.path.dirname(__file__), 'json')
 print(json_folder)
 model = tf.keras.models.load_model('detectorV2.h5')
@@ -56,30 +54,14 @@ def generate_events():
     print('EVENT')
     while True:
         data = get_latest_json()
-        if data:
+        if data and data.get('is_fraud') == 1:
             if data != last_json:
                 last_json = data
                 json_data = pd.DataFrame([data])
                 json_data = preProcess_data(json_data)
-                predict = model.predict(json_data)
-                predicted = (predict > 0.5).astype(float)
-                print(predicted)
-                if predicted[0][0] == 1.0:
-                    yield f"data: {json.dumps(data)}\n\n"
-        time.sleep(2)  # Comprueba cada 2 segundos si hay cambios en la carpeta JSON
+                prediction = model.predict(json_data)
+                if prediction[0][0] == 1.0:
+                    print(data)
+        time.sleep(2)  #
 
-@app.route('/')
-def mostrar_json():
-    data = get_latest_json()
-    return render_template('monitor.html', data=data)
-
-@app.route('/events')
-def sse():
-    return Response(generate_events(), content_type='text/event-stream')
-
-@app.route('/static/<path:filename>')
-def custom_static(filename):
-    return send_from_directory('static', filename)
-
-if __name__ == '__main__':
-    app.run(port=5001, threaded=True)
+generate_events()
